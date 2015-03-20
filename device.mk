c8d9bc61892607aec7fcaa36b8ea7ee149256fe5
Craig Gomez <craigacgomez@gmail.com>
2015-03-20 13:16:01 -0700

    Update build signature for 5.1 (LMY47D)

--- device.mk
+++ device.mk
@@ -236,8 +236,8 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=mantaray \
     BUILD_DISPLAY_ID="Lollipopalooza $(BUILD_ID) $(shell date +%Y%m%d)" \
-    BUILD_FINGERPRINT="google/mantaray/manta:5.0.1/LRX22C/1602158:user/release-keys" \
-    PRIVATE_BUILD_DESC="mantaray-user 5.0.1 LRX22C 1602158 release-keys"
+    BUILD_FINGERPRINT="google/mantaray/manta:5.1/LMY47D/1743759:user/release-keys" \
+    PRIVATE_BUILD_DESC="mantaray-user 5.1 LMY47D 1743759 release-keys"
 
 # for off charging mode
 PRODUCT_PACKAGES += \
